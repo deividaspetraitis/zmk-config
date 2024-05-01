8e7cc39f9cd0d96773a2c9a8ac2d8ba726eb65dd
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-05-01 18:25:14 +0300

    nine!view configuration init

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 CFG="/home/deividas/github.com/deividaspetraitis/zmk-config"
 
-west build -s "$CFG/zmk/app" -p -d "$CFG/build/right" -b nice_nano_v2 -- -DSHIELD=think_corne_right -DZMK_CONFIG="$CFG/config"
-west build -s "$CFG/zmk/app" -p -d "$CFG/build/left"  -b nice_nano_v2 -- -DSHIELD=think_corne_left -DZMK_CONFIG="$CFG/config"
+west build -s "$CFG/zmk/app" -p -d "$CFG/build/right" -b nice_nano_v2 -- -DSHIELD="think_corne_right nice_view_adapter nice_view" -DZMK_CONFIG="$CFG/config"
+west build -s "$CFG/zmk/app" -p -d "$CFG/build/left"  -b nice_nano_v2 -- -DSHIELD="think_corne_left nice_view_adapter nice_view" -DZMK_CONFIG="$CFG/config"
 west build -s "$CFG/zmk/app" -p -d "$CFG/build/reset" -b nice_nano_v2 -- -DSHIELD=settings_reset -DZMK_CONFIG="$CFG/config"
